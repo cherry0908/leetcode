d730504973cb20020bbf506479da7067a906a6b1
cherry.cao <cherry.cao@mitel.com>
2020-02-05 22:01:47 -0800

    Two Sum one iteration

--- 1-TwoSum.java
+++ 1-TwoSum.java
@@ -15,19 +15,38 @@ public class Main
                 if(a!=i){
                     indices[0]=a;
                     indices[1]=i;
-                    return indices;
+                    break;
                 }
             }
         }
+        return indices;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer,Integer>();
+        int i,a;
+        int[] indices = new int[2];
+        for(i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                a=map.get(nums[i]);
+                indices[0]=a;
+                indices[1]=i;
+                break;
+            }
+            else{
+                map.put(target-nums[i],i);
+            }
+        }
+        return indices;
     }
     
 	public static void main(String[] args) {
 	    Main m = new Main();
 	    int[] nums = {3,2,4};
-	    int[] nums2 = {2, 7, 11, 15};
-	    int[] r = m.twoSum(nums,6);
+	    int[] nums2 = {2,7,11,15};
+	    int[] r = m.twoSum2(nums,6);
         System.out.println(r[0] + ", " + r[1]);
-        r = m.twoSum(nums2,9);
+        r = m.twoSum2(nums2,9);
         System.out.println(r[0] + ", " + r[1]);
 	}
 }
