9eba08dc8ef2492b4dfe4a0c11f2713223bfbff3
cherry.cao <cherry.cao@mitel.com>
2020-02-07 14:14:22 -0800

    0077 combine

---
+++ 0077_combine.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.Arrays;
+
+public class Main
+{
+	
+	public void backtrack(int n, int k, int target, int start, ArrayList<Integer> current, List<List<Integer>> result) {
+    	if (target == 0) {
+    		ArrayList<Integer> tmp = new ArrayList<Integer>(current);
+    		result.add(tmp);
+    		return;
+    	}
+    	for (int i = start; i < n; i++) {
+    		current.add(i);
+    		backtrack(n, k, target-1, i + 1, current, result);
+    		current.remove(current.size() - 1);
+    	}
+    }
+    
+    public List<List<Integer>> combine(int n, int k) {
+    	List<List<Integer>> result = new ArrayList<List<Integer>>();
+    	if (n == 0) {
+    		return result;
+    	}
+    	
+    	ArrayList<Integer> current = new ArrayList<Integer>();
+    	backtrack(n+1, k, k, 1, current, result);
+    	return result;
+    }
+    
+	public static void main(String[] args) {
+	    Main m = new Main();
+        System.out.println("List: " + m.combine(4, 2));
+	}
+}
